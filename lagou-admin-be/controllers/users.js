49d8078d8657380b1185885e0549414b6452dc9c
wang994huan <775093289@qq.com>
2019-06-22 21:31:56 +0800

    后端密码加密出现问题

--- lagou-admin-be/controllers/users.js
+++ lagou-admin-be/controllers/users.js
@@ -1,10 +1,54 @@
-const userController = {
-    signup(req,res,next) {
-        res.set('Content-Type','application/json;charset=utf8')
-        res.render('succ',{
-            data: JSON.stringify(req.body)
+const userModel = require('../models/users')
+const bcrypt = require('bcrypt')
+
+class UserController {
+    // _hashPassword(pwd) {
+    //     return new Promise((resolve, reject) => {
+    //         bcrypt.hash(pwd, 10, (err, hash) => {
+    //             resolve(hash)
+    //         })
+    //     })
+
+    // }
+    _hashPassword(pwd) {
+        return new Promise((resolve) => {
+            bcrypt.hash(pwd, 10, (err, hash) => {
+                resolve(hash)
+            })
         })
+    }
+
+    _comparePassword() {
+
+    }
+
+    async signup(req, res, next) {
+        res.set('Content-Type', 'application/json;charset=utf8')
+
+        let password = req.body.password
+
+        let hash = await userController._hashPassword(password)
+
+        let result = await userModel.save({ ...req.body, password: hash })
+        
+
+        if (result) {
+            res.render('succ', {
+                data: JSON.stringify({
+                    message: '数据插入成功。'
+                })
+            })
+        } else {
+            res.render('fail', {
+                data: JSON.stringify({
+                    message: '数据插入失败。'
+                })
+            })
+        }
     }
 }
 
-module.exports =  userController
+
+const userController = new UserController()
+
+module.exports = userController
